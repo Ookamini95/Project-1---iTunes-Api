ee6dac199b7b6f778a08eb59dbb9a74df2150773
Ookamini95 <enrico.caminiti.95@gmail.com>
2022-07-24 16:56:30 +0100

    Additional functionality added

--- app.js
+++ app.js
@@ -1,52 +1,70 @@
 // Queries
 const testBtn = document.querySelector(".test");
-const quitBtn = document.querySelector(".quit__focus");
 const itemFocus = document.querySelector(".item__focus_box");
 const searchBox = document.querySelector(".search__box");
 const inputForm = document.querySelector(".search__form");
 const collectionBox = document.querySelector(".collection__box");
+const focusBox = document.querySelector(".item__focus_box");
 
-// Templates
+// Variables
+let dataArray = [];
 
 
 // Functions
 async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.results;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        dataArray = data.results;
+        // console.log(dataArray);
+        return data.results;
+    }
+    ////// Non sono sicuro se va bene così
+    catch (err) {
+        console.log(`error fetching data`);
+        return new Error(err);
+    }
 }
 
-// let temp;
-// getData().then(data => {
-//     temp = data
-//     console.log(data);
-//     console.log(temp);
-// });
 
 function search([name, type, num]) {
 
     const url = `https://itunes.apple.com/search?limit=${num}&media=${type.toLowerCase()}&term=${name}`
-    getData(url).then(dataObj => {
-        console.log(dataObj);
-        showCollection(dataObj)
-    })
+    getData(url)
+        .then(dataObj => {
+            // console.log(dataObj);
+            showCollection(dataObj);
+            return dataObj;
+        })
+        .catch(err => console.error(err))
+
     // console.log(data, data[0].artistName, data[0].artworkUrl100, data[0].trackCensoredName
 }
 
 function showCollection(data) {
     collectionBox.innerHTML = ``;
+    let index = 0;
     data.forEach(el => {
+
         let html = `
-        <div class="item">
+        <div class="item" data-index="${index}">
             <img src="${el.artworkUrl100}" class="img__item">
             <br>
-            <p>${el.artistName}</p>
+            <p><strong>${el.artistName.slice(0, 30)}</strong></p>
             <p>${el.trackCensoredName}</p>
         </div>
         `
+        index++;
         collectionBox.insertAdjacentHTML(`beforeend`, html);
     });
+}
 
+// TODO: Focus box implementation
+function loadOnFocus(data) {
+    console.log(data);
+    console.log(data.previewUrl);
+    const html = `<audio src="${data.previewUrl}" controls></audio>`;
+    focusBox.insertAdjacentHTML("beforeend", html);
 }
 
 //// Helper functions
@@ -57,13 +75,23 @@ function toggleFocus() {
 
 // Event Listeners
 ////  Focus box
-testBtn.addEventListener("click", toggleFocus);
-quitBtn.addEventListener("click", toggleFocus);
+///// Misc
+testBtn.addEventListener("click", toggleFocus); // TEST
 document.addEventListener("keydown", event => {
     if (event.key === `Escape` && !itemFocus.classList.contains("hidden")) toggleFocus();
 })
 
-////  Search bar misc
+///// Show focus
+collectionBox.addEventListener("click", event => {
+    const item = event.target.closest(".item")
+    const index = item.dataset.index
+    // loadOnFocus(dataArray[index]); WORKING!!
+    toggleFocus();
+    // console.log(dataArray[index])
+
+});
+
+////  Search bar
 inputForm.addEventListener(`submit`, event => {
     event.preventDefault();
     const queryInput = [...event.target].map(key => key.value);
@@ -71,3 +99,8 @@ inputForm.addEventListener(`submit`, event => {
     search(queryInput);
     event.target[0].value = ``;
 })
+
+
+// Additional TODOs
+//TODO: hover on text
+//TODO: fail to fetch => print
